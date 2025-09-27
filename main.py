ebac74464eb2c9c7490a35c19f8622a290857625
lykema-49 <lykema49@gmail.com>
2025-09-27 08:11:58 +0530

    ADDED CSV FILE

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-import pandas as pd
-
-def getTickerData(ticker):
-    df = pd.read_csv(f'stockData/{ticker}.csv')
-    return df
-
-if __name__ == '__main__':
-    data=getTickerData('HDFC')[['Date','Close']]
-    print(data.iloc['2020-09-11'])
+i
